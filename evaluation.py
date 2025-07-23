6cedf6cb9876d11761c531a502e37779683d3a06
Tarun Sheoran <121627136+TSS-sniper@users.noreply.github.com>
2025-07-24 04:06:29 +0530

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -8,8 +8,8 @@ def evaluate_summary(summary, reference):
     rouge_scores = rouge.get_scores(summary, reference)[0]
 
     return {
-        "bertscore_f1": round(F1.mean().item(), 4),
-        "rouge-1": rouge_scores["rouge-1"]["f"],
-        "rouge-2": rouge_scores["rouge-2"]["f"],
-        "rouge-l": rouge_scores["rouge-l"]["f"],
+        "BERTscore (F1)": round(F1.mean().item(), 4),
+        "ROUGE-1": rouge_scores["rouge-1"]["f"],
+        "ROUGE-2": rouge_scores["rouge-2"]["f"],
+        "ROUGE-L": rouge_scores["rouge-l"]["f"],
     }
